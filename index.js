ec225a61976ac566da3d9df4dc324043ac59a162
irfhan33 <irfhan025@gmail.com>
2021-11-20 17:43:36 +0800

    Second Update

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ let umur = "22 Tahun";
 
 hobi = "bermain games";
 
-// console.log(nama);
-console.log(
-  `nama saya adalah ${nama} dan hobi saya adalah ${hobi}. Tahun ini saya berumur`,
-  umur
-);
+function getNama() {
+  return nama;
+}
+
+console.log(getNama());
+
+// console.log(
+//   `nama saya adalah ${nama} dan hobi saya adalah ${hobi}. Tahun ini saya berumur`,
+//   umur
+// );
