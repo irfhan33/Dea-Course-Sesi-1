8cc3f01c7413e6e418dab742716d328fba86cb59
irfhan33 <irfhan025@gmail.com>
2021-11-20 17:15:33 +0800

    Trying to push for the first time

---
+++ index.js
@@ -0,0 +1,11 @@
+const nama = "muhammad irfan";
+let hobi = "Ngegame";
+let umur = "22 Tahun";
+
+hobi = "bermain games";
+
+// console.log(nama);
+console.log(
+  `nama saya adalah ${nama} dan hobi saya adalah ${hobi}. Tahun ini saya berumur`,
+  umur
+);
